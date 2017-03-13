b3ff7745067eb22efe1ec0238481d7062b3cafba
Michel Palourdio <mpalourdio@gmail.com>
2017-03-13 20:23:16 +0100

    varargs syntax

--- src/main/java/com/mpalourdio/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/mpalourdio/discoveryserver/DiscoveryServerApplication.java
@@ -17,7 +17,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryServerApplication {
 
-    public static void main(final String ...args) {
+    public static void main(final String... args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
