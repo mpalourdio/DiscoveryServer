75a69c6870cc9b6710f59e0535ce401f889dd724
Michel Palourdio <mpalourdio@gmail.com>
2025-11-01 14:56:13 +0100

    chore: polish SB4 migration

--- src/main/java/com/mpalourdio/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/mpalourdio/discoveryserver/DiscoveryServerApplication.java
@@ -15,9 +15,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServerApplication {
+class DiscoveryServerApplication {
 
-    public static void main(String... args) {
+    static void main(String... args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
