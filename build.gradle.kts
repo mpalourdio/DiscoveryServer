91138ffb10a5e80b732a7f9a5711532f9593d80b
Michel Palourdio <mpalourdio@gmail.com>
2026-02-07 16:47:32 +0100

    chore: Spring Cloud 2025.1.1 update

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2025.1.0"
+extra["springCloudVersion"] = "2025.1.1"
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
